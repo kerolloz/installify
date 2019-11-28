813e6c2b36273314c5058048b396ebe7f6550126
Kerollos <kerolloz@yahoo.com>
2019-11-28 19:58:46 +0200

    apt refactoring, full_upgrade

--- installer.sh
+++ installer.sh
@@ -7,10 +7,28 @@ BASEDIR=$(dirname "$0")
 programs=$(cat $BASEDIR/requirements.txt);
 echo "the following programs are going to be installed\n$programs"
 
-# Installing basic programs
-sudo apt update && sudo apt upgrade;
-sudo apt install $programs;
+function full_upgrade {
+    commands=(
+        update
+        upgrade
+        dist-upgrade
+        full-upgrade 
+        autoclean 
+        autoremove
+    )
 
+    for cmd in "${commands[@]}"; do
+        sudo apt $cmd -y
+    done
+}
+
+full_upgrade
+
+# Installing basic programs
+sudo apt install $programs -y; 
+# full_upgrade
+
+exit
 clear
 echo "Configuring git.."
 
