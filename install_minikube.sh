8fce95b2e196826650299b3c5ef22f5a182a026a
Eslam Adel <eslam.adel.me@gmail.com>
2021-07-08 21:25:25 +0200

    remove the `!` and reverse the logic

--- install_minikube.sh
+++ install_minikube.sh
@@ -8,20 +8,20 @@ fi
 
 install () {
 
-	if ! command kubectl >/dev/null 2>&1; then #	2>&1 simply says redirect standard error to standard output.
+	if command kubectl >/dev/null 2>&1; then #	2>&1 simply says redirect standard error to standard output.
+		echo "kubetcl is already installed"
+	else
 		curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 		chmod +x kubectl
 		mv kubectl $HOME/local/bin
-	else
-		echo "kubetcl is already installed"
 	fi
 
-	if ! command minikube >/dev/null 2>&1; then
+	if command minikube >/dev/null 2>&1; then
+		echo "minikube is already installed"
+	else
 		curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 		chmod +x minikube
 		mv minikube $HOME/local/bin
-	else
-		echo "minikube is already installed"
 	fi
 }
 
