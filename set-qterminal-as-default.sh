a2ebb7dffa8ab2749a19553f3463e5b0dea81d44
Kerollos Magdy <kerolloz@yahoo.com>
2021-11-13 00:13:53 +0200

    add scrtip to set QTerminal as default terminal

---
+++ set-qterminal-as-default.sh
@@ -0,0 +1,3 @@
+# sudo update-alternatives --set x-terminal-emulator /usr/bin/qterminal
+# sudo update-alternatives --auto x-terminal-emulator
+gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/qterminal
