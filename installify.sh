d89bd31a10125dc8f81731a5e6499c4f32499cfb
Kerollos Magdy <kerolloz@yahoo.com>
2021-11-12 12:05:38 +0200

    fix author name & enhance installify git config

--- installify.sh
+++ installify.sh
@@ -1,11 +1,15 @@
 clear;
+
 username=$(whoami);
 echo "Hello," $username;
 
 BASEDIR=$(dirname "$0")
-
 programs=$(cat $BASEDIR/requirements.txt);
-echo "the following programs are going to be installed\n$programs"
+
+echo "the following programs are going to be installed"
+echo "------------------------------------------------"
+echo "$programs"
+echo "------------------------------------------------"
 
 function full_upgrade {
     commands=(
@@ -24,22 +28,52 @@ function full_upgrade {
 
 full_upgrade
 
-# Installing software packages
+
+enter_fullscreen() {
+  # Save screen contents
+  tput smcup 2> /dev/null
+}
+
+leave_fullscreen() {
+  # Set cursor to normal
+  tput cnorm 2> /dev/null
+  # Restore screen contentsq
+  tput rmcup 2> /dev/null
+  stty echo
+}
+
+handle_sigint() {
+  leave_fullscreen
+  S="$?"
+  kill 0
+  exit $S
+}
+
+handle_sigtstp() {
+  leave_fullscreen
+  kill -s SIGSTOP $$
+}
+
+
+
 sudo apt install $programs -y; 
+echo "Installing software packages done."
 
-clear;
-
-echo "Copying templates..."
 cp -r $BASEDIR/templates/*  ~/Templates/
+echo "Copying templates done."
 
+enter_fullscreen
 clear
-echo "Configuring git..."
+trap handle_sigint INT
+trap handle_sigtstp SIGTSTP
 
 # Git configuration
 read -p 'Git Author Name: ' author_name
 read -p 'Git Email: ' email
 
 git config --global user.email $email
-git config --global user.name $author_name
+git config --global user.name "$author_name"
 
-echo "Done";
+leave_fullscreen
+echo "Git config done." 
+
