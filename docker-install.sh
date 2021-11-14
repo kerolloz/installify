a66f3fd3abf57912210a1f239607dc46d445027d
Kerollos Magdy <kerolloz@yahoo.com>
2021-11-14 11:39:09 +0200

    update docker-isntall.sh script

--- docker-install.sh
+++ docker-install.sh
@@ -1,19 +1,16 @@
 sudo apt remove docker docker-engine docker.io containerd runc
 sudo apt update
 sudo apt install \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER 
-
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
