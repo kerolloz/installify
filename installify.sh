2180ea6979d681d56dbcc5edfbe2659b1f6e607a
Kerollos Magdy <kerolloz@yahoo.com>
2022-09-28 18:43:51 +0200

    use ./updater.sh inside installify.sh

--- installify.sh
+++ installify.sh
@@ -11,23 +11,7 @@ echo "------------------------------------------------"
 echo "$programs"
 echo "------------------------------------------------"
 
-function full_upgrade {
-    commands=(
-        update
-        upgrade
-        dist-upgrade
-        full-upgrade 
-        autoclean 
-        autoremove
-    )
-
-    for cmd in "${commands[@]}"; do
-        sudo apt $cmd -y
-    done
-}
-
-full_upgrade
-
+./updater.sh
 
 enter_fullscreen() {
   # Save screen contents
