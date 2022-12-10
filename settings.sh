170102011cee2156e1ccf8b88554820cdc96bdfd
Kerollos Magdy <kerolloz@yahoo.com>
2022-12-10 02:58:42 +0200

    add settings.sh script

---
+++ settings.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.interface clock-format '12h'   
+gsettings set org.gnome.desktop.input-sources sources  "[('xkb', 'us'), ('xkb', 'ara')]" # add Arabic keyboard
+
