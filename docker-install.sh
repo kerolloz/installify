a0e3091db4538475705cff2b42a9488548a6457d
Kerollos Magdy <kerolloz@yahoo.com>
2022-12-10 03:20:20 +0200

    update docker-install script

--- docker-install.sh
+++ docker-install.sh
@@ -1,16 +1,32 @@
+# Older versions of Docker went by the names of docker, docker.io, or docker-engine. Uninstall any such older versions before attempting to install a new version:
 sudo apt remove docker docker-engine docker.io containerd runc
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt update
 sudo apt install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Add Docker’s official GPG key:
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Use the following command to set up the repository:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+
+# Update the apt package index
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+# Install Docker Engine, containerd, and Docker Compose.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Manage Docker as a non-root user
+
+# Create the docker group
 sudo groupadd docker
+# Add your user to the docker group
 sudo usermod -aG docker $USER
+# activate the changes to groups
 newgrp docker
