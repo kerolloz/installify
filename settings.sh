8fef1e17e06ec6b3e4180456a90d618e17b66213
Kerollos Magdy <kerolloz@yahoo.com>
2022-12-11 12:25:35 +0200

    add more to settings script

--- settings.sh
+++ settings.sh
@@ -1,4 +1,18 @@
+# Disable touchpad Natural Scroll
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+# 12 Hour clock Format
 gsettings set org.gnome.desktop.interface clock-format '12h'   
-gsettings set org.gnome.desktop.input-sources sources  "[('xkb', 'us'), ('xkb', 'ara')]" # add Arabic keyboard
+# Add Arabic keyboard
+gsettings set org.gnome.desktop.input-sources sources  "[('xkb', 'us'), ('xkb', 'ara')]" 
+# Show Battery Percentage
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+# Hide dock
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+# Enable Night Light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+# Show Weekday
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+# Hot corner
+gsettings set org.gnome.desktop.interface enable-hot-corners true
+
