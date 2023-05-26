fa22d60121735b3b957f9fa7c7b687b35d0ef97a
Kerollos Magdy <kerolloz@yahoo.com>
2023-05-27 01:13:18 +0300

    remove comments in set-qterminal-as-default.sh file

--- set-qterminal-as-default.sh
+++ set-qterminal-as-default.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# sudo update-alternatives --set x-terminal-emulator /usr/bin/qterminal
-# sudo update-alternatives --auto x-terminal-emulator
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/qterminal
