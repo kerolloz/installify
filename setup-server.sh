caf15a6eaec625f45358caea5aeac895e1b18554
Kerollos Magdy <kerolloz@yahoo.com>
2024-04-02 19:10:09 +0200

    Feat: setup-server.sh script

---
+++ setup-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Neovim
+sudo apt update
+sudo apt install neovim curl unzip -y
+
+# Install Oh My Bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Optionally, you might want to set Neovim as the default editor
+echo "export EDITOR=nvim" >> ~/.bashrc
+
+# Optionally, you might want to set Neovim as the default git editor
+echo "export GIT_EDITOR=nvim" >> ~/.bashrc
+./config-nvim.sh
+
+echo "Neovim and Oh My Bash have been installed successfully!"
+
