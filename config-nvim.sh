d55ed4f24e52e40d311a013a17c3992a60cd8e82
Kerollos Magdy <kerolloz@yahoo.com>
2024-04-02 17:31:42 +0200

    Update config-nvim.sh

--- config-nvim.sh
+++ config-nvim.sh
@@ -4,4 +4,4 @@ cp ./config/init.vim ~/.config/nvim/
 # Install Vundle
 [ ! -d ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 # Install plugins
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
