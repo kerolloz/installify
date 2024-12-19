348bd0b3d9472c8f7ae3da7de9b7898360e16d97
Kerollos Magdy <kerolloz@yahoo.com>
2024-12-19 14:00:54 +0200

    add install neovim script

---
+++ install-neovim.sh
@@ -0,0 +1,5 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
