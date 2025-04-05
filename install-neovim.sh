d0ef98d084c9142b0511ac44c765dcc4ce73d84a
Kerollos Magdy <kerolloz@yahoo.com>
2025-04-05 20:30:12 +0200

    Update install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,5 +1,14 @@
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+# Download the latest Neovim release tarball for Linux (x86_64)
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
+
+# Remove any existing Neovim installation
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
+
+# Extract the downloaded tarball to /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# Clean up the tarball
+rm nvim-linux-x86_64.tar.gz
+
+# Update the PATH in ~/.zshrc to include the Neovim binary
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.zshrc
